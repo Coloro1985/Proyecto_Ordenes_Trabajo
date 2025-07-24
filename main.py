dc2a9f146375dae1552ee3c98d712de670eaa1f8
Coloro1985 <claudioesteffan@gmail.com>
2025-07-24 19:47:39 -0400

    Refactor: reestructura de carpetas, limpieza y orden del proyecto

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 # Añadimos la raíz del proyecto al path
-root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+root_dir = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(root_dir)
 
 from interfaz_grafica import iniciar_interfaz
